20d4ec6f262799116a119dfae08a4cf47b78a6f6
CodeSandbox <support@codesandbox.io>
2023-09-25 11:52:53 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import DynamicForm from './Components/DynamicForm'
+export default function App() {
+  return (
+    <div className="App">
+      <DynamicForm />
+    </div>
+  );
+}
